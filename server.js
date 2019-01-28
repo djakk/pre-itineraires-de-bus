ba9140dc62affc20e165090f9ab0130282285f0a
Julein "djakk <julien.tadashi@gmail.com>
2019-01-28 19:34:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,18 +6,3 @@ mapnik.register_default_fonts();
 mapnik.register_default_input_plugins();
 
 var map = new mapnik.Map(256, 256);
-map.load('./test/stylesheet.xml', function(err,map) {
-    if (err) throw err;
-    map.zoomAll();
-    var im = new mapnik.Image(256, 256);
-    map.render(im, function(err,im) {
-      if (err) throw err;
-      im.encode('png', function(err,buffer) {
-          if (err) throw err;
-          fs.writeFile('map.png',buffer, function(err) {
-              if (err) throw err;
-              console.log('saved map image to map.png');
-          });
-      });
-    });
-});
