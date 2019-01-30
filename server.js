4970543c23f2093d721bfcacd645e8dd48dffd51
Julein "djakk <julien.tadashi@gmail.com>
2019-01-30 14:57:36 +0100

    test avec

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var stylesheet = './stylesheet.xml';
 http.createServer(function(req, res) {
   res.writeHead(500, {'Content-Type': 'text/plain'});
   
-  var req = queryOverpass('[out:json];(way(57.7,11.9,57.8,12.0)[railway];);(._;>;);out;', function(err, geojson) {
+  var req = queryOverpass('[out:json];(rel(57.7,11.9,57.8,12.0)[route];);(._;>;);out;', function(err, geojson) {
     if (err) {
       res.end(err.message);
     } else {
