8509fae86e953404c2aaeeb26d1c518c5c895d88
Julein "djakk <julien.tadashi@gmail.com>
2019-01-30 09:23:07 +0100

    First test points AND lines

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var stylesheet = './stylesheet.xml';
 http.createServer(function(req, res) {
   res.writeHead(500, {'Content-Type': 'text/plain'});
   
-  var req = queryOverpass('[out:json];node(57.7,11.9,57.8,12.0)[amenity=bar];out;', function(err, geojson) {
+  var req = queryOverpass('[out:json];node(57.7,11.9,57.8,12.0)[amenity=bar];way(57.7,11.9,57.8,12.0)[highway];out;', function(err, geojson) {
     if (err) {
       res.end(err.message);
     } else {
@@ -45,7 +45,7 @@ http.createServer(function(req, res) {
       var the_points_datasource = new mapnik.Datasource(options);
       var the_points_layer = new mapnik.Layer('points\' layer', "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs");
       the_points_layer.datasource = the_points_datasource;
-      the_points_layer.styles = ['points'];
+      the_points_layer.styles = ['points', 'lines'];
       map.add_layer(the_points_layer);
       
       map.zoomAll();
