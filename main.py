eb5e16b1d750bb9c2263db5f11e8788613a5a826
Julein "djakk <julien.tadashi@gmail.com>
2019-01-31 19:21:08 +0100

    will listen to queued tasks and process them as they are received

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import sys
+import os
 
 print "Coucou ! (from print)"
 sys.stdout.write("Coucou ! (from sys.stdout.write)")
 sys.stdout.flush()
+
+import rq
+print u"'rq' module loaded"
+import redis
+print u"'redis' module loaded"
+
+
+listen = ['high', 'default', 'low']
+
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+
+conn = redis.from_url(redis_url)
+
+if __name__ == '__main__':
+    with rq.Connection(conn):
+        worker = rq.Worker(map(rq.Queue, listen))
+        worker.work()
