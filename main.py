0e3f714559fdb2e12fbc80bc287c13edde66fb1f
Julein "djakk <julien.tadashi@gmail.com>
2019-02-01 19:39:24 +0100

    The urlparse module is renamed to urllib.parse in Python 3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 import pika
-import urlparse
+import urllib.parse
 
 print("Coucou ! (from print)")
 sys.stdout.write("Coucou ! (from sys.stdout.write)")
@@ -15,7 +15,7 @@ def aPrintingFunction(ch, method, properties, body):
 
 # Parse CLOUDAMQP_URL (fallback to localhost)
 url_str = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost//')
-url = urlparse.urlparse(url_str)
+url = urllib.parse.urlparse(url_str)
 params = pika.ConnectionParameters(host=url.hostname, virtual_host=url.path[1:],
     credentials=pika.PlainCredentials(url.username, url.password))
 
