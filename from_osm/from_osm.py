ae205b0b8ef94b713763d9a1613ded63de7e7ad8
Julein "djakk <julien.tadashi@gmail.com>
2019-02-02 15:21:07 +0100

    Created function 'get_data_from_osm()'

--- from_osm/from_osm.py
+++ from_osm/from_osm.py
@@ -5,7 +5,9 @@ import shapely.geometry
 import geopandas_osm
 import geopandas_osm.osm
 
-bbox = shapely.geometry.box(-1.6920,48.1506, -1.6753,48.1594)
-les_routes = geopandas_osm.osm.query_osm('way', bbox=bbox, recurse='down', tags='highway')
-les_routes = les_routes[les_routes.type == 'LineString'][['id', 'highway', 'name', 'geometry']].to_crs({'init': 'epsg:5837'}) # 5837 = 900913
-print les_routes.unary_union # à afficher dans qgis avec WKT
+def get_data_from_osm():
+  bbox = shapely.geometry.box(-1.6920,48.1506, -1.6753,48.1594)
+  the_roads = geopandas_osm.osm.query_osm('way', bbox=bbox, recurse='down', tags='highway')
+  the_roads = the_roads[the_roads.type == 'LineString'][['id', 'highway', 'name', 'geometry']].to_crs({'init': 'epsg:5837'}) # 5837 = 900913
+  print the_roads.unary_union # à afficher dans qgis avec WKT
+  return 0
