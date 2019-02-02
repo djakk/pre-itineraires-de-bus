606862f0b892d07242b31f705342f0135597675f
Julein "djakk <julien.tadashi@gmail.com>
2019-02-02 17:03:00 +0100

    Using a very small bbox

--- from_osm/from_osm.py
+++ from_osm/from_osm.py
@@ -6,8 +6,9 @@ import geopandas_osm
 import geopandas_osm.osm
 
 def get_data_from_osm():
-  bbox = shapely.geometry.box(-1.6920,48.1506, -1.6753,48.1594)
+  #bbox = shapely.geometry.box(-1.6920,48.1506, -1.6753,48.1594)
+  bbox = shapely.geometry.box(-1.6920,48.1506, -1.6910,48.1516) # very small bbox
   the_roads = geopandas_osm.osm.query_osm('way', bbox=bbox, recurse='down', tags='highway')
   the_roads = the_roads[the_roads.type == 'LineString'][['id', 'highway', 'name', 'geometry']].to_crs({'init': 'epsg:5837'}) # 5837 = 900913
-  #print(the_roads.unary_union) # à afficher dans qgis avec WKT
+  print(the_roads.unary_union) # à afficher dans qgis avec WKT
   return
