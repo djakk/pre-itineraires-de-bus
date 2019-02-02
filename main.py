d6b6c5b52930730921d70b49bb7eee2496556e76
Julein "djakk <julien.tadashi@gmail.com>
2019-02-02 14:14:25 +0100

    Added durable=False to basic_consume() args
    Will it work ?

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ channel = connection.channel() # start a channel
 print(u"channel.basic_consume …")
 channel.basic_consume(aPrintingFunction,
     queue='myQueue2',
-    no_ack=True)
+    no_ack=True, 
+    durable=False)
 
 print(u"channel.start_consuming …")
 channel.start_consuming() # start consuming (blocks)
