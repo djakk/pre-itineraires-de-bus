f837c48d34e269aa49a5518b7346e51a0e205671
Julein "djakk <julien.tadashi@gmail.com>
2019-02-02 23:40:33 +0100

    First attempt INSERT INTO

--- osm_io/to_postgresql.py
+++ osm_io/to_postgresql.py
@@ -1,6 +1,24 @@
 import psycopg2
 
-def save_to_postgresql(the_datas, the_url_to_the_database):
+def save_to_postgresql(the_osm_datas, the_url_to_the_database):
+  """
+  psql $DATABASE_URL -> 
+    CREATE EXTENSION postgis;
+    CREATE EXTENSION hstore;
+    
+    CREATE TABLE myTable (osm_id  bigint, properties  hstore, geometry  geometry);
+  """
+  print("inside 'save_to_postgresql'")
   the_connection = psycopg2.connect(the_url_to_the_database)
+  
+  the_cursor = the_connection.cursor()
+  the_cursor.execute("""DELETE FROM myTable;""")
+  the_cursor.close()
+  
+  the_cursor = the_connection.cursor()
+  the_cursor.executemany("""INSERT INTO myTable (osm_id) VALUES (%(id)s);""", the_osm_datas)
+  the_cursor.close()
+  
+  the_connection.commit()
   the_connection.close()
   return
