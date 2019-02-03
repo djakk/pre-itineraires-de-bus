b7410a2ace984b538cd096e2c642abbb61d3747d
Julein "djakk <julien.tadashi@gmail.com>
2019-02-03 08:31:24 +0100

    Trying .dropna() instead of filtering on dict values (which does not work …
    … because Nan is converted to a string ("nan") !

--- osm_io/to_postgresql.py
+++ osm_io/to_postgresql.py
@@ -21,14 +21,14 @@ def save_to_postgresql(the_osm_datas, the_url_to_the_database):
   the_cursor.execute("""DELETE FROM myTable;""")
   the_cursor.close()
   
-  the_osm_datas_as_records = the_osm_datas.to_dict('records')
+  the_osm_datas_as_records = the_osm_datas.dropna().to_dict('records')
   for a_record in the_osm_datas_as_records:
     
     # extract the non-null properties of the record, except id and geometry
     the_record_as_a_copy = dict(a_record)
     del the_record_as_a_copy["id"]
     del the_record_as_a_copy["geometry"]
-    the_record_as_a_copy = { a_key : a_value for a_key, a_value in the_record_as_a_copy.items() if a_value.notnull()} # nan = null
+    #the_record_as_a_copy = { a_key : a_value for a_key, a_value in the_record_as_a_copy.items() if a_value.notnull()} # nan = null
     a_record["properties"] = the_record_as_a_copy
     
     # work on the geometry column
