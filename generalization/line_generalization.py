a6fa5f4ce2dc3ea9fb8539b2cab0f7be13a0e906
Julein "djakk <julien.tadashi@gmail.com>
2019-02-04 16:34:14 +0100

    ".geometry = " -> can't set attribute -> "['geometry'] = "

--- generalization/line_generalization.py
+++ generalization/line_generalization.py
@@ -20,7 +20,7 @@ def generalize(the_geometries, the_generalization_size):
     for a_slice in range(0, the_number_of_slices_to_be_done +1 +1):
       the_geometry_as_a_point = copy.copy(a_geometry)
       the_fraction = a_slice / ( float(the_number_of_slices_to_be_done) +1 )
-      the_geometry_as_a_point.geometry = a_geometry.geometry.interpolate(the_fraction, normalized=True)
+      the_geometry_as_a_point['geometry'] = a_geometry.geometry.interpolate(the_fraction, normalized=True)
       the_geometry_as_a_point['the_position'] = a_slice +1
       the_geometries_generalized__as_a_list.append(the_geometry_as_a_point)
   
