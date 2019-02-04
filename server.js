8b6c692f90e1beeef36b14e9ca4801c9ebc49ad2
Julein "djakk <julien.tadashi@gmail.com>
2019-02-04 14:19:52 +0100

    Removed query-overpass (replaced by python's querying)

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var mapnik = require('mapnik');
 var http = require('http');
 //const url = require('url');
 
-var queryOverpass = require('query-overpass');
-
 var ampqlib = require('amqplib');
 
 
@@ -63,82 +61,61 @@ http.createServer(function(req, res) {
   
   res.writeHead(500, {'Content-Type': 'text/plain'});
   
-  var req = queryOverpass('[out:json];(rel(57.7,11.9,57.8,12.0)[route=tram][ref=3];rel(57.7,11.9,57.8,12.0)[route=tram][ref=10];);(._;>;);out;', function(err, geojson) {
+  
+  // map with just a style
+  // eventually the api will support adding styles in javascript (!)
+  var s = '<Map srs="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs">';
+  s += '<Style name="points">';
+  s += ' <Rule>';
+  s += '  <PointSymbolizer />';
+  s += ' </Rule>';
+  s += '</Style>';
+  s += '<Style name="lines">';
+  s += ' <Rule>';
+  s += '  <LineSymbolizer stroke="[colour]" />';
+  s += ' </Rule>';
+  s += '</Style>';
+  s += '</Map>';
+  
+  // create map object
+  var map = new mapnik.Map(1024, 1024);
+  map.fromStringSync(s);
+  
+  console.log("creating the map …");
+  
+  the_database_url = new URL(process.env.DATABASE_URL);
+  var options = {
+    type: 'postgis',
+    host: the_database_url.hostname, 
+    port: the_database_url.port, 
+    dbname: the_database_url.pathname.substring(1), 
+    user: the_database_url.username,
+    password: the_database_url.password, 
+    table: "(SELECT geometry, 'red' AS colour FROM mytable) AS roads", 
+    geometry_field: 'geometry'
+  };
+  
+  var the_points_datasource = new mapnik.Datasource(options);
+  var the_points_layer = new mapnik.Layer('points\' layer', "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs");
+  the_points_layer.datasource = the_points_datasource;
+  the_points_layer.styles = ['lines', 'points'];
+  map.add_layer(the_points_layer);
+  
+  console.log("creating the image …");
+  map.zoomAll();
+  var the_image__for_the_map = new mapnik.Image(1024, 1024);
+  map.render(the_image__for_the_map, function(err,im) {
     if (err) {
       res.end(err.message);
     } else {
-      console.log("the overpass query has been done");
-      console.log(geojson.features.length);
-      
-      for (var a_feature_index = 0; a_feature_index < geojson["features"].length; a_feature_index++) {
-        var a_feature = geojson["features"][a_feature_index];
-        var the_colour_of_the_relation;
-        try {
-          the_colour_of_the_relation = a_feature["properties"]["relations"][0]["reltags"]["colour"];
-        } catch { };
-        if (the_colour_of_the_relation) {
-          //a_feature["properties"]["tags"]["colour"] = the_colour_of_the_relation;
-          a_feature["properties"]["colour"] = the_colour_of_the_relation;
-          //a_feature["colour"] = the_colour_of_the_relation;
-        }
-      }
-      
-      // map with just a style
-      // eventually the api will support adding styles in javascript (!)
-      var s = '<Map srs="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs">';
-      s += '<Style name="points">';
-      s += ' <Rule>';
-      s += '  <PointSymbolizer />';
-      s += ' </Rule>';
-      s += '</Style>';
-      s += '<Style name="lines">';
-      s += ' <Rule>';
-      s += '  <LineSymbolizer stroke="[colour]" />';
-      s += ' </Rule>';
-      s += '</Style>';
-      s += '</Map>';
-      
-      // create map object
-      var map = new mapnik.Map(1024, 1024);
-      map.fromStringSync(s);
-      
-      console.log("creating the map …");
-      
-      the_database_url = new URL(process.env.DATABASE_URL);
-      var options = {
-        type: 'postgis',
-        host: the_database_url.hostname, 
-        port: the_database_url.port, 
-        dbname: the_database_url.pathname.substring(1), 
-        user: the_database_url.username,
-        password: the_database_url.password, 
-        table: "(SELECT geometry, 'red' AS colour FROM mytable) AS roads", 
-        geometry_field: 'geometry'
-      };
-      
-      var the_points_datasource = new mapnik.Datasource(options);
-      var the_points_layer = new mapnik.Layer('points\' layer', "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs");
-      the_points_layer.datasource = the_points_datasource;
-      the_points_layer.styles = ['lines', 'points'];
-      map.add_layer(the_points_layer);
-      
-      console.log("creating the image …");
-      map.zoomAll();
-      var the_image__for_the_map = new mapnik.Image(1024, 1024);
-      map.render(the_image__for_the_map, function(err,im) {
+      im.encode('png', function(err,buffer) {
         if (err) {
           res.end(err.message);
         } else {
-          im.encode('png', function(err,buffer) {
-            if (err) {
-              res.end(err.message);
-            } else {
-              res.writeHead(200, {'Content-Type': 'image/png'});
-              res.end(buffer);
-            }
-          });
+          res.writeHead(200, {'Content-Type': 'image/png'});
+          res.end(buffer);
         }
       });
     }
-  }, { flatProperties: false });
+  });
 }).listen(port);
