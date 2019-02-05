2a605692d4a121eb4c5cdcee5a7c653b9dd764ff
Julein "djakk <julien.tadashi@gmail.com>
2019-02-05 14:23:37 +0100

    .groupby().first() -> what does it ?

--- osm_io/from_osm.py
+++ osm_io/from_osm.py
@@ -21,7 +21,7 @@ def get_data_from_osm():
   the_properties_as_name.remove('id')
   the_properties_as_name.remove('geometry')
   print(the_properties_as_name)
-  the_roads = the_roads.groupby(by=the_properties_as_name, level=0, axis=1)
+  the_roads = the_roads.groupby(by=the_properties_as_name, level=0, axis=1).first()
   print("just after '.groupby' : ")
   print(list(the_roads))
   print(the_roads)
