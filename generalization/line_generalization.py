12541a8e6fbfe164c83604743c3dbc57b4cbf68b
Julein "djakk <julien.tadashi@gmail.com>
2019-02-05 12:13:18 +0100

    added a comment

--- generalization/line_generalization.py
+++ generalization/line_generalization.py
@@ -7,6 +7,7 @@ import pandas
 def generalize(the_geometries, the_generalization_size):
   
   the_geometries['geometry'] = the_geometries['geometry'].apply(from_one_line_to_several_points, args=(the_generalization_size,))
+  # from list inside a row to several rows, in a pandas' way
   # help : https://mikulskibartosz.name/how-to-split-a-list-inside-a-dataframe-cell-into-rows-in-pandas-9849d8ff2401 : 
   the_geometries_2 = the_geometries.geometry.apply(pandas.Series) \
     .merge(the_geometries, left_index = True, right_index = True) \
