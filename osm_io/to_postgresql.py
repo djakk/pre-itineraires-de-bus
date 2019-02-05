da475a26790fecf952ae64174983df9ad25f5dec
Julein "djakk <julien.tadashi@gmail.com>
2019-02-05 16:42:57 +0100

    Updating to_postgresql.py according to update of from_osm.py

--- osm_io/to_postgresql.py
+++ osm_io/to_postgresql.py
@@ -6,11 +6,13 @@ import pandas
 
 def save_to_postgresql(the_osm_datas, the_url_to_the_database):
   """
+  the_osm_datas : only 4 columns : id (=osm_id), type (=node, way or relation), properties (=dictionary {"highway": "secondary", "name": "blablabla"}), geometry
+  
   psql $DATABASE_URL -> 
     CREATE EXTENSION postgis;
     CREATE EXTENSION hstore;
     
-    CREATE TABLE mytable (osm_id  bigint, properties  hstore, geometry  geometry);
+    CREATE TABLE "mytable" ("osm_id"  bigint, "type"  text, "properties"  hstore, "geometry"  geometry);
   """
   print("inside 'save_to_postgresql'")
   print(the_osm_datas)
@@ -23,22 +25,13 @@ def save_to_postgresql(the_osm_datas, the_url_to_the_database):
   the_cursor.execute("""DELETE FROM myTable;""")
   the_cursor.close()
   
-  # .to_dict('records') convert NaN to "nan" instead of None, so we have to convert NaN to None before
-  # (this changes the dtype of all columns to object)
-  the_osm_datas_as_records = the_osm_datas.where((pandas.notnull(the_osm_datas)), None).to_dict('records')
+  the_osm_datas_as_records = the_osm_datas.to_dict('records')
   
   the_counter = 1
   for a_record in the_osm_datas_as_records:
     
     if the_counter >= 10000:
       break
-    
-    # extract the non-None properties of the record, except id and geometry
-    the_record_as_a_copy = dict(a_record)
-    del the_record_as_a_copy["id"]
-    del the_record_as_a_copy["geometry"]
-    the_record_as_a_copy = { a_key : a_value for a_key, a_value in the_record_as_a_copy.items() if a_value is not None}
-    a_record["properties"] = the_record_as_a_copy
     
     # work on the geometry column
     a_record["geometry"] = a_record["geometry"].wkb_hex
@@ -49,8 +42,8 @@ def save_to_postgresql(the_osm_datas, the_url_to_the_database):
     psycopg2.extras.register_hstore(the_cursor)
     the_cursor.execute("""\
 INSERT INTO mytable 
-       (osm_id,   geometry,     properties) 
-VALUES (%(id)s, %(geometry)s, %(properties)s);\
+       ("osm_id", "type", "geometry", "properties") 
+VALUES (%(id)s, %(type)s, %(geometry)s, %(properties)s);\
 """, a_record)
     the_cursor.close()
     
