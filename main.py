a40f1d8b1638ebd1432234ae8a5b0bf9ed6ac898
Julein "djakk <julien.tadashi@gmail.com>
2019-02-05 17:13:59 +0100

    Added logs

--- main.py
+++ main.py
@@ -29,10 +29,17 @@ def aPrintingFunction(ch, method, properties, body):
 
 def theCallbackFunction(ch, method, properties, body):
     print(u"inside theCallbackFunction")
+    
     the_datas = osm_io.from_osm.get_data_from_osm()
+    print("after osm_io.from_osm.get_data_from_osm()" : )
+    print(list(the_datas))
+    print(the_datas)
+    
     the_datas_2 = generalization.line_generalization.generalize(the_datas, 10)
+    
     # geopandas -> postgresql database
     osm_io.to_postgresql.save_to_postgresql(the_datas_2, os.environ.get('DATABASE_URL'))
+    
     # datas are ready inside the postgresql database : send a response back to NodeJS
     the_connection = pika.BlockingConnection(params)
     the_channel = the_connection.channel()
